32d1e1cd45a872b1b859d3c31041ac6c98d44e71
Felix Girke <felix.girke@tu-dortmund.de>
2017-02-02 18:52:31 +0100

    initial commit
    added restpusher, restmuell and license

---
+++ restmuell.py
@@ -0,0 +1,88 @@
+#Copyright © 2017 razorheadfx <razorhead.effect@gmail.com>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar.
+#See http://www.wtfpl.net/ or LICENSE for more details.
+
+from restpusher import RestPusher
+import json
+from cmd import Cmd
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+frm = '[%(levelname)s] %(module)s.%(funcName)s - %(message)s'
+logging.basicConfig(stream=sys.stdout, level = logging.INFO, format = frm)
+
+class RESTMuell(Cmd):
+    """ a trashy commandline wrapper for foaas.com's REST API... as a (dis-)service """
+    def __init__(self):
+        Cmd.__init__(self)
+        self.rp = RestPusher("www.foaas.com")
+        
+        "pull in FOaaS' self description"
+        ret = self.rp.get("/operations")
+        operation_def = ret["body"]
+        
+        """
+        example from the API self description
+        {"name":"Who the fuck are you anyway","url":"/anyway/:company/:from","fields":[{"name":"Company","field":"company"},{"name":"From","fie ld":"from"}]}
+        """
+        
+        "generate do_<operation> functions like Cmd expects them and glue them to this class"
+        for jsonNode in operation_def:
+            url = jsonNode["url"]
+            
+            if "-" in url:
+                continue
+            
+            url = url.replace("/", "").split(":")[0]
+            
+            fields = self.extract_fields(jsonNode)
+            name = jsonNode["name"]
+            self.make_method(url, name, fields)
+            
+    def extract_fields(self, json):
+        fields = []
+        for fieldset in json["fields"]:
+            fields.append(fieldset["field"])
+        
+        return fields
+    
+    def make_method(self, operation, name, fields = ["none"]):
+        """ add a method to the FOaaS class and name do_<operation> """
+        """TODO: add argcount checking"""
+        methodtemplate = "def do_%s(self, args):\n    '''%s args: %s\n    prints: %s'''\n    self._get('%s',args)\nRESTMuell.do_%s=do_%s"
+        
+        exec(methodtemplate %(operation,len(fields),fields,name,operation,operation,operation))
+
+           
+    def _get(self, operation, args = ""):
+        """
+        get request
+        """
+        url = "/"+operation
+        if args != "":
+            url = url+"/"+args.replace(' ', '/')
+            
+        ret = self.rp.get(url = url)
+        
+        print "%s" %ret["body"]["message"]
+        print "      -%s" %ret["body"]["subtitle"]
+        return 
+    
+    def do_exit(self, args):
+        """exit the shell"""
+        print "---- disposing of this trash and exiting."
+        sys.exit(0)
+
+if __name__ == "__main__":
+    
+    muell = RESTMuell()
+    muell.prompt = "fuck off as a service>"
+    log.debug("%s" %dir(muell)) #log the class signature so we can see which methods have been added
+    print "Welcome to RESTMuell the Fuck off-as-a-Service (foaas.com) shell"
+    print "---- enter 'help [COMMAND]' to bring up the command reference"
+    muell.cmdloop()
+    
+    
---
+++ restpusher.py
@@ -0,0 +1,93 @@
+#Copyright © 2017 razorheadfx <razorhead.effect@gmail.com>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar.
+#See http://www.wtfpl.net/ or LICENSE for more details.
+
+import httplib
+import json
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+frm = '[%(levelname)s] %(module)s.%(funcName)s - %(message)s'
+logging.basicConfig(stream=sys.stdout, level = logging.WARN, format = frm)
+
+class RestPusher:
+    """ a simple wrapper for post/put/get/delete http requests"""
+    def __init__(self, server, port = 80):
+        self.server = server
+        self.port = port
+    
+    def set_server(self, server):
+        """
+        change the target hostname/ip addr to the given one
+        :param server, address of the server
+        """
+        assert(type(serveraddress) == str)
+        self.server = serveraddress
+    
+    def get(self, url, data = None, headers = None):
+        """ perform a get request
+        :param url
+        :param object/dict, which will be dumped using json.dumps
+        :param dict of header settigs i.e.  Content-type : application/json
+        """
+        
+        ret = self._rest_call(url, data, 'GET', headers)
+        return ret
+  
+    def post(self, url, data):
+        
+        ret = self._rest_call(url, data, "POST", headers = None)
+        return ret
+    
+    def put(self, url, data, headers = None):
+        """ perform a put request
+        :param url
+        :param object/dict, which will be dumped using json.dumps
+        :param dict of header settigs i.e.  Content-type : application/json
+        """
+        ret = self._rest_call(url, json, "PUT", headers = None)
+        return ret
+    
+    def delete(self, url, data = None, headers = None):
+        ret = self._rest_call(url, data, "DELETE", headers)
+        return ret
+  
+    def _rest_call(self, url, data, action, headers = None):
+        assert(action in ["GET","PUT","POST","DELETE"])
+        assert(url != None)
+        
+        """use json by default"""
+        if headers == None:
+            headers = {"Content-type": "application/json","Accept": "application/json"}
+            
+        if data != None:
+            reqbody = data.dumps(data)
+        else:
+            reqbody = None
+        
+        log.debug("Calling to " +self.server+url)
+        
+        con = httplib.HTTPConnection(self.server, self.port)
+        con.request(action, url, body = reqbody, headers =  headers)
+        response = con.getresponse()        
+        
+        if response.status != 200:
+            log.error("Non 200 statuscode: %s for request %s:%s/%s\n body:%s headers: %s" %(response.status,action, self.server, url, reqbody, headers))
+        
+        
+        try:
+            body = json.loads(response.read())
+                    
+        except:
+            body = None
+            log.error("An error occoured while parsing the returned JSON")
+        
+        resp_data = {"statuscode": response.status, "body" : body, "headers" : response.getheaders()}
+        log.debug("%s" %resp_data)
+
+        con.close()
+        
+        return resp_data
